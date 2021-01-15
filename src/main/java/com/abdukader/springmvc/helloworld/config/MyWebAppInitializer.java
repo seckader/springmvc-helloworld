000697481c89e3d50b2da2203c04bf138806a0c7
Abdou Kader SECK <seckader.pro@gmail.com>
2021-01-15 19:42:44 +0000

    Servlet Container Initialization

---
+++ src/main/java/com/abdukader/springmvc/helloworld/config/MyWebAppInitializer.java
@@ -0,0 +1,22 @@
+package com.abdukader.springmvc.helloworld.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class MyWebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return new Class<?>[0];
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return new Class<?>[] { AppConfig.class };
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] { "/" };
+	}
+
+}
