e215387bced9f48463f8d1a651513de132f39b50
Abdou Kader SECK <seckader.pro@gmail.com>
2021-01-15 19:34:31 +0000

    Spring Configuration

---
+++ src/main/java/com/abdukader/springmvc/helloworld/config/AppConfig.java
@@ -0,0 +1,30 @@
+package com.abdukader.springmvc.helloworld.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@EnableWebMvc
+@Configuration
+@ComponentScan(basePackages = "com.abdukader.springmvc.helloworld")
+public class AppConfig {
+
+	/*
+	 * En XML, cette configuration se mettra dans le fichier webmvc-config.xml
+	 */
+
+	@Bean
+	public InternalResourceViewResolver resolver() {
+
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		resolver.setViewClass(JstlView.class);
+		resolver.setPrefix("/WEB-INF/views/");
+		resolver.setSuffix(".jsp");
+
+		return resolver;
+	}
+
+}
