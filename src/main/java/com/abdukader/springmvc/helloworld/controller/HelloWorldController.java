f0ea687e4c66935e3b99f8a975fcfd873064aefa
Abdou Kader SECK <seckader.pro@gmail.com>
2021-01-15 22:06:05 +0000

    Fix issues, Build, Deploy & Run

--- src/main/java/com/abdukader/springmvc/helloworld/controller/HelloWorldController.java
+++ src/main/java/com/abdukader/springmvc/helloworld/controller/HelloWorldController.java
@@ -10,20 +10,20 @@ import com.abdukader.springmvc.helloworld.domain.HelloWorld;
 
 @Controller
 public class HelloWorldController {
-	
-	private static final String HANDLER_ATTRIBUTE = "helloworld";
-	
+
+	private static final String VIEW = "index";
+
 	@GetMapping(path = "/helloworld")
 	public String handler(Model model) {
-		
+
 		HelloWorld helloWorld = new HelloWorld();
-		
+
 		helloWorld.setMessage("Hello World Example Using Spring MVC !!!");
 		helloWorld.setDate(LocalDate.now());
-		
-		model.addAttribute(HANDLER_ATTRIBUTE, helloWorld);
-		
-		return HANDLER_ATTRIBUTE;
+
+		model.addAttribute("helloWorld", helloWorld);
+
+		return VIEW;
 	}
 
 }
